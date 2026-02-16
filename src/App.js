a989246fe7cd34a640a866f5238b7a63b472be75
ahmadharthey <ahmoood@outlook.com>
2026-02-16 17:22:22 +0300

    added routes for transactions

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import { ThemeProvider,createTheme } from '@mui/material';
+import Dashboard from './pages/Dashboard';
+import TechnicianServices from './pages/TechnicianServices';
+import WorkOrder from './pages/WorkOrder'
 const router = createBrowserRouter([
   {path:"/",element:<SignIn/>},
-  {},
-  {}])
+  {path:"/dashboard",element:<Dashboard/>},
+  {path:"/transactions/workorder", element:<WorkOrder/>},
+  {path:"/transactions/technicianservices", element:<TechnicianServices/>}
+])
 const theme = createTheme({
   palette:{
     mode:"dark",
